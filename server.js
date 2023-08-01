c16761cfe611dacc77803c812b8035f36f4826f0
Narasimhan1989 <40212780+Narasimhan1989@users.noreply.github.com>
2023-08-01 23:12:37 +0530

    node server

---
+++ server.js
@@ -0,0 +1,13 @@
+var http = require('http')
+var fs = require('fs')
+
+const PORT = 8080
+
+fs.readFile('./index.html', function(error, html) {
+    if(error) throw error;
+    http.createServer(function(request, response) {
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write(html);
+        response.end();
+    }).listen(PORT)
+});
